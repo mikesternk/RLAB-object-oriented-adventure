3190d2938a01ed8417d9506988107c275f5a6312
mikesternk <mikesternk@gmail.com>
2024-02-29 11:01:07 -0600

    Part 2

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Part 1
+// PART 1
 const adventurer = {
   name: "Robin",
   health: 10,
@@ -37,5 +37,28 @@ adventurer.companion.companion = {
   inventory: ["small hat", "sunglasses"],
 };
 
-adventurer.roll();
+// adventurer.roll();
 // console.log(adventurer);
+
+// PART 2
+class Character {
+  constructor(name) {
+    this.name = name;
+    this.health = 100;
+    this.inventory = [];
+  }
+  roll(mod = 0) {
+    const result = Math.floor(Math.random() * 20) + 1 + mod;
+    console.log(`${this.name} rolled a ${result}.`);
+  }
+}
+const robin = new Character("Robin");
+robin.inventory = ["sword", "potion", "artifact"];
+robin.companion = new Character("Leo");
+robin.companion.type = "Cat";
+robin.companion.companion = new Character("Frank");
+robin.companion.companion.type = "Flea";
+robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+robin.companion.companion.roll();
+// console.log(robin);
